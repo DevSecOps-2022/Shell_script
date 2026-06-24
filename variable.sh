51185ed4b317aa84bc09e005fa73e54b1a96b2f0
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-24 19:34:36 +0530

    writing shell script

--- variable.sh
+++ variable.sh
@@ -4,13 +4,13 @@ Timestamp=$(date)
 
 echo "Time is $Timestamp"
 
-Start_time=$(date+%s)
+Start_time=$(date + %s)
 
 sleep 10
 
-End_time=$(date+%s)
+End_time=$(date + %s)
 
-Final_time=$(($End_time - $Start_time))
+Final_time=$(($End_time-$Start_time))
 
 echo "Total script executed time is $Final_time"
 
