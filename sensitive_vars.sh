c964f1c1d86fb547bd36804ac22b3b47e9aea1f6
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-24 19:27:45 +0530

    writing shell script

---
+++ sensitive_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter the user name..."
+
+read user_name
+
+echo "given user name is $user_name"
+
+echo "Please enter the password for user name..."
+
+read -s Password
+
+echo "Given Password is $Password"
