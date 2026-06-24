a24652f8e51161afa6343d396f883171e0b924ca
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-24 20:51:12 +0530

    writing shell script

---
+++ loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter a number....."
+
+read number
+
+if [ $number -lt 10]; then
+    echo "Given $number is less than 10"
+elif
+    [ $number -gt 10]; then
+    echo "Given $number is greater than 10"
+else
+    echo "Gien $number is equal to 10"
