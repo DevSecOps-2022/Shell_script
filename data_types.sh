ec9d5d4be89f760faf03a3a432a6265d81da7d36
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-24 20:41:44 +0530

    writing shell script

---
+++ data_types.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("RRR" "VARANASI" "PUSHPA")
+
+echo "Movies are: ${MOVIES[@]}"
+echo "First Movie is:${MOVIES[0]}"
+echo "Second Movie is:${MOVIES[1]}"
+echo "Third Movie is:${MOVIES[2]}"
