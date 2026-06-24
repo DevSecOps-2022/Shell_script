e1cf2c2d249d8bdb357848051e2480fa9230e955
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-24 19:20:18 +0530

    writing shell script

---
+++ conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$person1=Bubu
+$person2=Dudu
+
+echo "$person1: Hi $person2, How are you......"
+echo "$person2: good $person1, How are you...."
+echo "$person1: good $person2, I am missing you $person1...."
+echo "$person2: really....????$person1,  Love U .......... $person1...."
