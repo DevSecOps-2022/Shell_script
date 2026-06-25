38e6d8f90a7aa703b650071341685552d3a82b30
Hanumanth Reddy <nhanumanthreddy897@.com>
2026-06-25 13:46:54 +0530

    updated file

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+# check whether user is root or not
+
+if [ $userid -ne 0 ]; then
+    echo "Please run this script with root user"
+fi
+
+# check software is already installed 
+
+dnf list installed mysql
+
+if [ $? eq 0 ]; then
+    echo "software is alredy installed"
+else
+    echo "Installing software"
+    dnf install mysql -y
+
+    if  [ $? eq 0 ]; then
+        echo "MYSQL software is installed ......SUCCESS"
+    else
+        echo "MYSQL software is installation .......FAILED"
+    fi
+
+fi
